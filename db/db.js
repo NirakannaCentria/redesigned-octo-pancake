85606297313203d0dda69a9629415042f8fa947c
NirakannaCentria <akaengstrom@gmail.com>
2024-11-24 18:39:57 +0200

    Add files via upload

--- db/db.js
+++ db/db.js
@@ -1,12 +1,14 @@
-//This is the database connection setup using PostgreSQL in Deno.
 import { Client } from "https://deno.land/x/postgres/mod.ts";
+
 // Set up PostgreSQL client connection
 const client = new Client({
-user: "cyberuser",
-database: "cyber_sec_db",
-hostname: "localhost",
-password: "cybersecpassword",
-port: 5432,
+  user: "postgres",
+  database: "postgres",
+  hostname: "localhost",
+  password: "12345!",
+  port: 5432,
 });
+
 await client.connect();
+
 export default client;
